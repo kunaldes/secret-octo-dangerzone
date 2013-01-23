345302da4f0eb90f262e2565177905871340d393
James Grugett <jahooma@gmail.com>
2013-01-22 21:50:01 -0500

    Changed tabs to spaces.

--- game.js
+++ game.js
@@ -2,24 +2,24 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var game = function() {
-	var exports = {};
-	
-	var FPS = 60;
-	var i = 0;
-	function gameLoop() {
-		i = (i + 1) % FPS;
-		
-		ctx.clearRect(0, 0, 800, 600);
-		ctx.fillStyle = "orange";
-		ctx.fillRect(300 + i * 2,200, 100, 100);	
-	}
-	
-	exports.start = function() {
-		console.log("game started");
-		setInterval(gameLoop, 1000/FPS);
-	}
-	
-	return exports;
+    var exports = {};
+    
+    var FPS = 60;
+    var i = 0;
+    function gameLoop() {
+        i = (i + 1) % FPS;
+        
+        ctx.clearRect(0, 0, 800, 600);
+        ctx.fillStyle = "orange";
+        ctx.fillRect(300 + i * 2,200, 100, 100);
+    }
+    
+    exports.start = function() {
+        console.log("game started");
+        setInterval(gameLoop, 1000/FPS);
+    }
+    
+    return exports;
 }();
 
 game.start();
